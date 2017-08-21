717df1b7094278a33e21cacefa78b4acecd092e4
Logan Rakai <lmrakai@gmail.com>
2017-08-21 12:47:56 -0600

    Remove artifacts function

--- post-deploy/bootstrap.ps1
+++ post-deploy/bootstrap.ps1
@@ -1,7 +1,3 @@
-function Set-LabArtifacts {
-    $ProgressPreference = 'SilentlyContinue' # Ignore progress updates (100X speedup)
-}
-
 function Disable-InternetExplorerESC {
     $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
     $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
@@ -31,6 +27,5 @@ Set-MpPreference -DisableRealtimeMonitoring $true
 # Disable Windows update
 Stop-Service -displayname "Windows Update"
 
-Set-LabArtifacts
 Disable-UserAccessControl
 Disable-InternetExplorerESC
